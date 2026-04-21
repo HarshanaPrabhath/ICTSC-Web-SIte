97d519c2b37ca44f4eba68f8b30d210d404954f8
HarshanaPrabhath <harshanaprabhath147@gmail.com>
2026-04-21 19:27:07 +0530

    changes images

--- Frontend/ictsc web/src/pages/Team.jsx
+++ Frontend/ictsc web/src/pages/Team.jsx
@@ -6,7 +6,7 @@ import Footer from "../components/Footer";
 
 // Executive
 import Chirath from "../assets/executive/Chirath.jpg";
-import HarshanaImg from "../assets/executive/Harshana.jpg";
+import Harshana from "../assets/executive/Harshana.jpg";
 import Sajini from "../assets/executive/sajini.jpg";
 import Shonali from "../assets/executive/shonali.jpg";
 import Niroda from "../assets/executive/Niroda.jpg";
@@ -82,7 +82,7 @@ function Team() {
     vicePresident: {
       name: "Harshana Prabhath",
       role: "Vice President",
-      image: HarshanaImg,
+      image: Harshana,
     },
     secretaries: [
       { name: "Sajini Kaushalya", role: "Joint Secretary", image: Sajini },
